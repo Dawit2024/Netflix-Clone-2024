acce6d4575a387c76c5fdd44c7906d0317847bb7
dawitkassa <unitefortruth80@gmail.com>
2024-04-28 16:04:59 -0500

    Header and Footer

--- netflix-clone/src/Components/Header/Header.js
+++ netflix-clone/src/Components/Header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./Header.css"
-import Netflixlogo from '../../Assets/Images/NETFLIXLOGO.jpeg'
+import Netflix1 from "../../Assets/Images/Netflix1.jpeg";
 import SearchIcon from "@mui/icons-material/Search";
 import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
@@ -16,32 +16,43 @@ import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 
 const Header = () => {
   return (
-    <div className='header_outer_container'>
-        <div className='header_container'>
-            <div className='header_left'>
-                <ul>
-                    <li> <img src={Netflixlogo} alt='Netflix logo' width="100"></img></li>
+    <div className="header_outer_container">
+      <div className="header_container">
+        <div className="header_left">
+          <ul>
+            <li>
+              {" "}
+              <img src={Netflix1} alt="Netflix logo" width="100"></img>
+            </li>
 
-                    <li>Home</li>
-                    <li>TvShows</li>
-                    <li>Movies</li>
-                    <li>Latest</li>
-                    <li>MyLists</li>
-                    <li>Browse by Language</li>
-                </ul>
-            </div>
-            <div className='header-right'>
-            <ul className='headerIcon'> 
-                <li><SearchIcon /></li>
-                    <li> <NotificationsNoneIcon /></li>
-                    <li><AccountBoxIcon /></li> 
-                    <li><ArrowDropDownIcon /></li>
-                
-                </ul>
-            </div>
+            <li>Home</li>
+            <li>TvShows</li>
+            <li>Movies</li>
+            <li>Latest</li>
+            <li>MyLists</li>
+            <li>Browse by Language</li>
+          </ul>
         </div>
+        <div className="header-right">
+          <ul className="headerIcon">
+            <li>
+              <SearchIcon />
+            </li>
+            <li>
+              {" "}
+              <NotificationsNoneIcon />
+            </li>
+            <li>
+              <AccountBoxIcon />
+            </li>
+            <li>
+              <ArrowDropDownIcon />
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Header
